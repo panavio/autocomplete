eaf7a0d5152edb29efae16c1d50ce27e95d3487e
Patricia Krisanti <127014039+panavio@users.noreply.github.com>
2023-05-08 05:35:14 -0400

    Add files via upload

---
+++ autocomplete/autocomplete.h
@@ -0,0 +1,29 @@
+#if !defined(AUTOCOMPLETE_H)
+#define AUTOCOMPLETE_H
+
+typedef struct term{
+    char term[200]; // assume terms are not longer than 200
+    double weight;
+} term;
+
+
+void read_in_terms(struct term **terms, int *pnterms, char *filename);
+int lowest_match(struct term *terms, int nterms, char *substr);
+int highest_match(struct term *terms, int nterms, char *substr);
+void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr);
+
+#endif
+
+// #if !defined(asdlakjdlask)
+// #define asdlakjdlask
+
+// #endif
+/*
+
+class hello:
+def __init__(hello):
+
+
+
+
+*/
